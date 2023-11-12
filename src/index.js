00bac40c3bf203c45949124c5ae9a473055fddd4
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-11-12 14:04:02 -0800

    Link makeFooter module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import header from "./makeHeader";
 import main from "./makeMain";
+import footer from "./makeFooter";
 
 const content = document.getElementById("content");
 
-content.append(header, main);
+content.append(header, main, footer);
 
