113e06c3b7919834bb43e2c9693b076bfcf374a8
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-11-12 14:03:23 -0800

    Finish module

---
+++ src/makeFooter.js
@@ -0,0 +1,8 @@
+function footer() {
+    const mainElement = document.createElement("footer");
+    mainElement.textContent = "possible credits?";
+
+    return mainElement;
+}
+
+export default footer();
