6fdf5dd8406f3c7a020993712d5ad3e896446be6
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-11-14 21:14:37 -0800

    Add tab switching module functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import header from "./makeHeader";
 import main from "./makeMain";
 import footer from "./makeFooter";
+import home from "./renderHome";
 
 const content = document.getElementById("content");
 
 content.append(header, main, footer);
+
+const display = document.querySelector("#menu");
+const homeTab = document.querySelector("#tabsmenu > li:nth-child(1)");
+const contactTab = document.querySelector("#tabsmenu > li:nth-child(2)");
+const menuTab = document.querySelector("#tabsmenu > li:nth-child(3)");
+
+homeTab.addEventListener("click", function() { 
+    display.replaceChildren();
+    display.appendChild(home);
+})
+
+contactTab.addEventListener("click", function() { 
+    display.replaceChildren();
+})
+
+menuTab.addEventListener("click", function() { 
+    display.replaceChildren();
+})
 
