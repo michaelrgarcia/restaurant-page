dee27cf31143300232103eda80909dfb36782800
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-11-14 19:53:21 -0800

    Add tabs menu to makeMain.js

--- src/makeMain.js
+++ src/makeMain.js
@@ -1,10 +1,25 @@
 function main() {
     const mainElement = document.createElement("main");
 
+    const tabs = document.createElement("ul");
+    tabs.id = "tabsmenu";
+
+    for (let i = 0; i < 3; i++) {
+        let test = document.createElement("li");
+        if (i === 0) {
+            test.textContent = "Home";
+        } else if (i === 1) {
+            test.textContent = "Contact";
+        } else if (i === 2) {
+            test.textContent = "Menu"
+        }
+        tabs.appendChild(test);
+    } 
+
     const scrollMenu = document.createElement("div");
-    scrollMenu.id = "catalog"
-    scrollMenu.textContent = "I am the tab switching menu";
-    
+    scrollMenu.id = "menu"
+
+    mainElement.appendChild(tabs);
     mainElement.appendChild(scrollMenu);
 
     return mainElement;
